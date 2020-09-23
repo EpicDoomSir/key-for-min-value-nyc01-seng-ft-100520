bcc21b8c0c94c6a8c1746fd774f431bd364cb386
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-23 19:19:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if smallest_value > value
       smallest_value = value
+      key_of_smallest_value = key
     end
   end
-  key_of_smallest_value = name_hash.key(smallest_value)
+  key_of_smallest_value
 end
