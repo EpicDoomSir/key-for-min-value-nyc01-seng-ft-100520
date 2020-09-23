9b1495c64a8dc715ccf1779b566c4b6e9a047b63
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-23 19:17:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,11 @@
 
 def key_for_min_value(name_hash)
   key_of_smallest_value = ""
-  
+  smallest_value = 99999
   name_hash.each do |key, value|
-    
+    if smallest_value > value
+      smallest_value = value
+    end
   end
+  key_of_smallest_value = name_hash.key(smallest_value)
 end
